d8928a630abb46463550ffb1caa282c91249a77e
Gkp83401 <99257658+Gkp83401@users.noreply.github.com>
2023-06-14 08:01:42 +0800

    add command

--- SourceFile/envCLI/envCLI.c
+++ SourceFile/envCLI/envCLI.c
@@ -9,59 +9,68 @@
 
 unsigned char envCLI(envLoginOutImformation *aEnvLoginOutImformationPoint)
 {
+    // 存放获取到的一行命令
     superstring command;
+    // 用于存放计算结果
     int calResult;
+    // 存放把数字转化为的字符串
     char string[20];
+    
+    // 清屏
     screen_w(0, 0x01);
+    // 显示提示符
     my_printf("$ ");
     
     while (1) {
+        // 对command进行初始化
         command.str[0] = 0;
         command.index = 0;
         command.scrIndex = 0;
         
+        // 判断是否存在复位按键
         if (settleOneLine(&command) == TO_RESET)
             return TO_RESET;
         
+        // 是否为命令right
         if (strCmpNoIndex(command.str, "right")) {
             screen_w(0, 0x01);
             my_printf("$ \nright");
             screen_w(0, 0x80 + 0x02);
         } else if (strCmpNoIndex(command.str, "plus")) {
-            screen_w(0, 0x01);
+            screen_w(0, 0x01);      // 是否为命令plus
             my_printf("$ \n character: +");
             screen_w(0, 0x80 + 0x02);
         } else if (strCmpNoIndex(command.str, "emoji")) {
-            screen_w(0, 0x01);
+            screen_w(0, 0x01);      // 是否为命令emoji
             my_printf("$ \n character: ");
             screen_w(1, 0xEF);
             screen_w(0, 0x80 + 0x02);
         } else if (strFindPlusAndCal(command.str, &calResult)) {
-            screen_w(0, 0x01);
+            screen_w(0, 0x01);      // 是否为加法运算
             numToStr(calResult, string);
             my_printf("$ \nresult: ");
             my_printf(string);
             screen_w(0, 0x80 + 0x02);
         } else if (strFindJianAndCal(command.str, &calResult)) {
-            screen_w(0, 0x01);
+            screen_w(0, 0x01);      // 是否为减法运算
             numToStr(calResult, string);
             my_printf("$ \nresult: ");
             my_printf(string);
             screen_w(0, 0x80 + 0x02);
         } else if (strFindChengAndCal(command.str, &calResult)) {
-            screen_w(0, 0x01);
+            screen_w(0, 0x01);      // 是否为乘法运算
             numToStr(calResult, string);
             my_printf("$ \nresult: ");
             my_printf(string);
             screen_w(0, 0x80 + 0x02);
         } else if (strFindChuAndCal(command.str, &calResult)) {
-            screen_w(0, 0x01);
+            screen_w(0, 0x01);      // 是否为除法运算
             numToStr(calResult, string);
             my_printf("$ \nresult: ");
             my_printf(string);
             screen_w(0, 0x80 + 0x02);
         } else if (strFindPingAndCal(command.str, &calResult)) {
-            screen_w(0, 0x01);
+            screen_w(0, 0x01);      // 是否为次方运算
             numToStr(calResult, string);
             my_printf("$ \nresult: ");
             my_printf(string);
--- SourceFile/envLogin/envLogin.c
+++ SourceFile/envLogin/envLogin.c
@@ -10,23 +10,40 @@
 
 unsigned char envLogin(envLoginOutImformation *aEnvLoginOutImformationPoint)
 {
+    // 存放输入的用户名
     superstring user = {"", 0, 0};
+    // 存放输入的密码
     superstring passwd = {"", 0, 0};
     
     while(1) {
+        // 清屏
         screen_w(0, 0x01);
+        // 向屏幕输入"user:"
+        // 然后回车接着输入"passwd:"
         my_printf("user:\npasswd:");
         
+        // 这时光标来到第2行"passwd"的后面
+        
+        // 使光标去到第1行"user:"后面
         screen_w(0, 0x80 + 0x05);
         
+        // 调用settleOneLine函数来将输入存到user中
+        // 如果函数返回值等于TO_RESET代表按下了重启键
+        // 所以条件成立会用return结束当前函数，
+        // 然后去到main函数让程序重新运行
         if (settleOneLine(&user) == TO_RESET)
             return TO_RESET;
         
+        // 使光标去到第2行"passwd:"的后面
         screen_w(0, 0x80 + 0x40 + 0x07);
         
+        // 调用settleOneLine函数来将输入存到passwd中
         if (settleOneLinePasswd(&passwd) == TO_RESET)
             return TO_RESET;
         
+        // 判断user和passwd输入的正确与否
+        // 如果正确用return返回正确信号
+        // 如果不正确while(1）会使其从头开始执行
         if (strCmpNoIndex(user.str, "abc") && strCmpNoIndex(passwd.str, "123")) {
             return NORMAL_RETURN;
         }
